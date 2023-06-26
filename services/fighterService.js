9bda4512024979452f8c237987b36f9d7d4ea496
yaroslavDp <bayern744@gmail.com>
2023-06-26 22:39:36 +0300

    fighterService methods implemented

--- services/fighterService.js
+++ services/fighterService.js
@@ -2,6 +2,33 @@ import { fighterRepository } from "../repositories/fighterRepository.js";
 
 class FighterService {
   // TODO: Implement methods to work with fighters
+      getFighters() {
+        return fighterRepository.getAll();
+      }
+
+      getFighterById(id) {
+        return fighterRepository.getOne({ id });
+      }
+
+      createFighter(data) {
+        return fighterRepository.create(data);
+      }
+
+      updateFighter(id, data) {
+          return fighterRepository.update(id, data);
+      }
+
+      deleteFighter(id) {
+          return fighterRepository.delete(id);
+      }
+
+      search(search) {
+          const item = fighterRepository.getOne(search);
+          if (!item) {
+              return null;
+          }
+          return item;
+      }
 }
 
 const fighterService = new FighterService();
