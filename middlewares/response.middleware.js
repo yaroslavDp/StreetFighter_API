a9ad8c9f3086f247c050279f8dbd80b8a6c6902b
yaroslavDp <bayern744@gmail.com>
2023-06-26 22:44:44 +0300

    responseMiddleware implemented

--- middlewares/response.middleware.js
+++ middlewares/response.middleware.js
@@ -1,5 +1,20 @@
 const responseMiddleware = (req, res, next) => {
   // TODO: Implement middleware that returns result of the query
+
+      const { data, err } = res;
+      const sendResponse = (data) => {
+        return res.status(200).json({status: 'Success', data});
+      };
+
+      const sendError = (message, status = 400) => {
+        return res.status(status).json({ error: true, message });
+      };
+
+      if (err) {
+          sendError(err.message, err.statusCode)
+      } else if (data) {
+        sendResponse(data);
+      }
   next();
 };
 
