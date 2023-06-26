d1c139f48f7029f0049f1e451d062dae4b415dfe
yaroslavDp <bayern744@gmail.com>
2023-06-26 22:40:31 +0300

    userService methods implemented

--- services/userService.js
+++ services/userService.js
@@ -2,14 +2,33 @@ import { userRepository } from "../repositories/userRepository.js";
 
 class UserService {
   // TODO: Implement methods to work with user
+      getUsers() {
+        return userRepository.getAll();
+      }
 
-  search(search) {
-    const item = userRepository.getOne(search);
-    if (!item) {
-      return null;
-    }
-    return item;
-  }
+      getUserById(id) {
+          return userRepository.getOne({ id });
+      }
+
+      createUser(data) {
+          return userRepository.create(data);
+      }
+
+      updateUser(id, data) {
+          return userRepository.update(id, data);
+      }
+
+      deleteUser(id) {
+          return userRepository.delete(id);
+      }
+
+      search(search) {
+        const item = userRepository.getOne(search);
+        if (!item) {
+          return null;
+        }
+        return item;
+      }
 }
 
 const userService = new UserService();
